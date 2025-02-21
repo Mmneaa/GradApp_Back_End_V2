b1295d96e74729589242113f18b527e16c6ef7a2
Mahmoud Nabil <126608589+Mmneaa@users.noreply.github.com>
2025-02-21 15:03:30 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGODB_URI, {});
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error("MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
